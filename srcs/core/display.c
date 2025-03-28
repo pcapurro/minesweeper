a6f1b51edfd97b792c795d914c4a38b15bde4603
pcapurro <fossa634@gmail.com>
2025-03-28 15:33:40 +0100

    added displayMap(), map is now displayed according to the values of map variable

--- srcs/core/display.c
+++ srcs/core/display.c
@@ -1,5 +1,30 @@
 #include "header.h"
 
+void	displayMap(tInfos* infos)
+{
+	SDL_Rect	obj;
+
+	obj.x = 15;
+	obj.y = 60;
+
+	for (int i = 0; infos->map[i] != NULL; i++)
+	{
+		for (int k = 0; infos->map[i][k] != '\0'; k++)
+		{
+			obj.w = (infos->width * 30) / infos->width;
+			obj.h = (infos->height * 30) / infos->height;
+
+			obj.x = 15 + (k * obj.w);
+
+			SDL_SetRenderDrawColor(infos->mainRenderer, getRandomNumber(), getRandomNumber(), getRandomNumber(), 255);
+			SDL_RenderFillRect(infos->mainRenderer, &obj);
+
+			SDL_RenderCopy(infos->mainRenderer, NULL, NULL, &obj);
+		}
+		obj.y = 60 + (i * obj.h);
+	}
+}
+
 void	displayGame(tInfos* infos)
 {
 	SDL_Rect	obj;
@@ -11,6 +36,8 @@ void	displayGame(tInfos* infos)
 	SDL_RenderFillRect(infos->mainRenderer, &obj);
 
 	SDL_RenderCopy(infos->mainRenderer, NULL, NULL, &obj);
+
+	displayMap(infos);
 
 	SDL_RenderPresent(infos->mainRenderer);
 }
