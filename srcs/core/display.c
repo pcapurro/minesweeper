19cc597c2a399eede0eaf1de61bbed8c5a5e639b
pcapurro <fossa634@gmail.com>
2025-04-10 15:49:19 +0200

    numbers displayed on map are now with valid proportions

--- srcs/core/display.c
+++ srcs/core/display.c
@@ -25,19 +25,20 @@ SDL_Texture*	getTexture(tInfos* infos, const int value)
 
 void	displayMap(tInfos* infos)
 {
-	SDL_Rect	obj;
+	SDL_Rect	cellObj;
+	SDL_Rect	textObj;
 
-	obj.x = 21;
-	obj.y = 70;
+	cellObj.x = 21;
+	cellObj.y = 70;
 
 	for (int i = 0; infos->map[i] != NULL; i++)
 	{
 		for (int k = 0; k != infos->width; k++)
 		{
-			obj.w = (infos->width * 42) / infos->width;
-			obj.h = (infos->height * 42) / infos->height;
+			cellObj.w = (infos->width * 42) / infos->width;
+			cellObj.h = (infos->height * 42) / infos->height;
 
-			obj.x = 21 + (k * obj.w);
+			cellObj.x = 21 + (k * cellObj.w);
 
 			if (infos->map[i][k].bomb == false && infos->map[i][k].value == 0)
 				continue ;
@@ -47,14 +48,23 @@ void	displayMap(tInfos* infos)
 			if (infos->map[i][k].bomb == true)
 			{
 				SDL_SetRenderDrawColor(infos->mainRenderer, 255, 0, 0, 255);
-				SDL_RenderFillRect(infos->mainRenderer, &obj);
+				SDL_RenderFillRect(infos->mainRenderer, &cellObj);
+
+				SDL_RenderCopy(infos->mainRenderer, texture, NULL, &cellObj);
 			}
 			else
+			{
 				texture = getTexture(infos, infos->map[i][k].value);
 
-			SDL_RenderCopy(infos->mainRenderer, texture, NULL, &obj);
+				textObj.w = 21 * (infos->width / 16), textObj.h = 52 * (infos->height / 16);
+
+				textObj.x = cellObj.x + ((cellObj.w / 2) - (textObj.w / 2));
+				textObj.y = cellObj.y + cellObj.h / 2 - (textObj.h / 2);
+
+				SDL_RenderCopy(infos->mainRenderer, texture, NULL, &textObj);
+			}
 		}
-		obj.y = 70 + ((i + 1) * obj.h);
+		cellObj.y = 70 + ((i + 1) * cellObj.h);
 	}
 }
 
